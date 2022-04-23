538d8a3a20f43fde35afe31d617b7d1d0b5a84f0
Shaoli Lu <shaoli_lu@yahoo.co>
2022-04-23 18:14:31 -0500

    Add project files.

---
+++ EventManagementApi/Services/IEventRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace EventManagementApi.Services
+{
+    public interface IEventRepository
+    {
+        Event Add(Event newEvent);
+        IEnumerable<Event> GetAll();
+
+        Event GetById(int id);
+
+        void Delete(int id);
+    }
+}
